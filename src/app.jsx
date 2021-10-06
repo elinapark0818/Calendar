d933e25a7bb02afdd7827e0c376e31bc1c67eb0f
Elina <elinapark0818@gmail.com>
2021-10-07 03:16:28 +0900

    Full Calendar

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,12 @@
 import styles from './app.module.css';
-import Aside from './components/Aside/aside';
-import CalendarControl from './components/calendar/calendar-control';
-import Footer from './components/Footer/footer';
-import Header from './components/Header/header';
 
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className={styles.app}>
-      <header>
-        <h1>캘린더를 만들어봅시다.</h1>
-        <Header/>
-      </header>
-      <main>
-        <Aside/>
-        <CalendarControl/>
-      </main>
-      <footer>
-        <Footer/>
-      </footer>
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
---
+++ src/components/calendar/calendar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './calendar.module.css';
+
+export default function Calendar() {
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
