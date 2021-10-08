45616e2e1a9f9e2423dc4ddbbe5e5513ea2cbcf8
Elina <elinapark0818@gmail.com>
2021-10-08 22:31:47 +0900

    component

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,12 @@
 import styles from './app.module.css';
 
 import React from 'react'
+import Calendar from './components/calendar/calendar';
 
 export default function App() {
   return (
     <div>
-      
+      <Calendar/>
     </div>
   )
 }
--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import styles from './calendar.module.css';
+// import styles from './calendar.module.css';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
 
-export default function Calendar() {
-  
+export default function CalendarControl() {
+
   return (
     <div>
+      <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
       
+      events={[
+        { title: "event 1", date: "2021-10-08" },
+        { title: "event 2", date: "2020-09-08" }
+      ]}/>
     </div>
   )
 }
