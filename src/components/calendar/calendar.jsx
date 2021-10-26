3e2b946bf1403ade65e9bcef370a8df2d5931ff6
Elina <elinapark0818@gmail.com>
2021-10-26 17:26:10 +0900

    fix full-calendar

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 // import styles from './calendar.module.css';
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import listPlugin from '@fullcalendar/list';
-import "@fullcalendar/core/main.css";
-import "@fullcalendar/daygrid/main.css";
-import "@fullcalendar/timegrid/main.css";
+
 
 export default function CalendarControl() {
 
