993a692d50eafe04c72875685e8bd85317c62a4a
Elina <elinapark0818@gmail.com>
2021-10-26 18:05:12 +0900

    calendar resize

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,21 +1,9 @@
-import React from 'react';
-// import styles from './calendar.module.css';
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from '@fullcalendar/daygrid';
+import React from 'react'
 
-
-export default function CalendarControl() {
-
+export default function Calendar() {
   return (
     <div>
-      <FullCalendar
-      plugins={[dayGridPlugin]}
-      initialView="dayGridMonth"
       
-      events={[
-        { title: "event 1", date: "2021-10-08" },
-        { title: "event 2", date: "2020-09-08" }
-      ]}/>
     </div>
   )
 }
